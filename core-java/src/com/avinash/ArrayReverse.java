28c00b881a0f4bb0d270df9c1837fd445811b0bf
Avinash Tingre <avinash.tingre@cerence.com>
2021-05-31 22:41:46 +0530

    update ArrayReverse.java

--- core-java/src/com/avinash/ArrayReverse.java
+++ core-java/src/com/avinash/ArrayReverse.java
@@ -8,12 +8,30 @@ public class ArrayReverse {
         String str = "avinash";
         char[] input = str.toCharArray();
 
-        Object[] result = reverseArray(input);
+        Object[] result1 = reverseArrayType1(input);
+        char[] result2 = reverseArrayType2(input, 0, input.length - 1);
 
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result1));
+        System.out.println(Arrays.toString(result2));
     }
 
-    private static Object[] reverseArray(char[] input) {
+    // More efficient because it only traverse half the elements. No need to create separate array for storing result.
+    private static char[] reverseArrayType2(char[] input, int start, int end) {
+        char temp;
+
+        while (start < end) {
+            temp = input[start];
+            input[start] = input[end];
+            input[end] = temp;
+            end--;
+            start++;
+        }
+
+        return input;
+
+    }
+
+    private static Object[] reverseArrayType1(char[] input) {
         Object[] result = new Object[input.length];
 
         for (int i = input.length - 1; i >= 0; i--) {
