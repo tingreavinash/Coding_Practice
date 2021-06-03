6096065d4bb1d2aa1108fb02cbfe36c4080ae7ce
Avinash Tingre <avinash.tingre@cerence.com>
2021-06-03 13:28:26 +0530

    Problem: Print all prime numbers less than or equal to given number.

--- core-java/src/com/avinash/PrimeNumber.java
+++ core-java/src/com/avinash/PrimeNumber.java
@@ -1,24 +1,34 @@
 package com.avinash;
 
 /***
- * Problem: Check if given number is prime or not.
+ * Problem: Print all prime numbers less than or equal to given number.
+ * Note: this is slower than sieve of Eratosthenes algorithm.
  */
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int number = 4;
+        long starttime = System.currentTimeMillis();
 
-        if (isPrime(number)) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is NOT a prime number.");
+        printPrimeNumbers(100);
+        long endtime = System.currentTimeMillis();
+        System.out.println("\nTime taken: " + (endtime - starttime));
+
+    }
+
+    public static void printPrimeNumbers(int number) {
+        int start = 2;
+        while (start <= number) {
+            if (isPrime(start)) {
+                System.out.print(start + " ");
+            }
+            start++;
         }
     }
 
     public static boolean isPrime(int n) {
 
         int divisor = 2;
-        while (divisor < n) {
+        while (divisor * divisor <= n) {
             if (n % divisor == 0) {
                 return false;
             }
