6735f39a75b20e20858bb31da43338d02e62ffb4
Avinash Tingre <avinash.tingre@cerence.com>
2021-06-06 00:08:01 +0530

    Problem: Add all numbers from 0 to n.

---
+++ core-java/src/com/avinash/RangeSum.java
@@ -0,0 +1,32 @@
+package com.avinash;
+
+/**
+ * Problem: Add all numbers from 0 to n.
+ */
+
+public class RangeSum {
+
+    public static void main(String[] args) {
+
+        long t1 = System.currentTimeMillis();
+        System.out.println("Sum is: " + addUptoOptimized(555555555));
+        long t2 = System.currentTimeMillis();
+        System.out.println("Time taken for Optimized: " + (t2 - t1));
+
+        System.out.println("Sum is: " + addUpto(555555555));
+        long t3 = System.currentTimeMillis();
+        System.out.println("Time taken for Normal: " + (t3 - t2));
+    }
+
+    public static long addUpto(int num) {
+        long total = 0;
+        for (int i = 0; i <= num; i++) {
+            total = total + i;
+        }
+        return total;
+    }
+
+    public static long addUptoOptimized(int num) {
+        return ((long) num * (num + 1)) / 2;
+    }
+}
