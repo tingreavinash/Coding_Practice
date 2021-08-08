52ac24ff947c14ad2de9ee3a6489dec7e5c0828e
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-08 10:01:05 +0530

    Fundamentals practice

---
+++ core-java/src/com/avinash/fundamentals/ConsoleInput.java
@@ -0,0 +1,17 @@
+package com.avinash.fundamentals;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+    public static void main(String[] args) {
+        String input_string = readInputFromConsole();
+        System.out.println("Input value: " + input_string);
+    }
+
+    private static String readInputFromConsole() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter some value: ");
+        String str = sc.nextLine();
+        return str;
+    }
+}
