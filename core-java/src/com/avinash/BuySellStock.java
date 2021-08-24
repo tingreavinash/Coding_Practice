110aa7d3237a563c5f7e9ebe88da2aa9060a6d15
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-24 21:55:15 +0530

    LC 121 - Best Time to Buy and Sell Stock

---
+++ core-java/src/com/avinash/BuySellStock.java
@@ -0,0 +1,23 @@
+package com.avinash;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+public class BuySellStock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = maxProfit(prices);
+        System.out.println(result);
+    }
+
+    private static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+}
