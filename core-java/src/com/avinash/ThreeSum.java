96150697f6b4d3c3a36a3c64d2652fd518d192e4
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-25 15:34:44 +0530

    LC 15 - three sum (faster)

--- core-java/src/com/avinash/ThreeSum.java
+++ core-java/src/com/avinash/ThreeSum.java
@@ -1,9 +1,6 @@
 package com.avinash;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/3sum
@@ -12,54 +9,73 @@ import java.util.List;
 public class ThreeSum {
 
     public static void main(String[] args) {
-        //int[] nums = {-1, 0, 1, 2, -1, -4};
+        //int[] nums = {-1,0,1,2,-1,-4};
         //Expected: [[-1,-1,2],[-1,0,1]]
 
         int[] nums = {-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4};
         //Expected: [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
 
-        List<List<Integer>> res = threesum(nums);
+        //List<List<Integer>> res = threesum(nums);
+        List<List<Integer>> res = threesum1(nums);
         System.out.println(res);
     }
 
-    private static List<List<Integer>> threesum(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
+    // faster algorithm
+    private static List<List<Integer>> threesum1(int[] nums) {
+        Arrays.sort(nums);                                      // O(NlogN)
+        List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int firstNum = nums[i];
-            int diff = -nums[i];
-            HashMap<Integer, Integer> map = new HashMap<>();
-            int[] arr = new int[2];
+        for (int i = 0; i < nums.length - 2; i++) {               // O(N)
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            for (int j = i + 1; j < nums.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                int currValue = nums[j];
-                Integer k = map.get(diff - currValue);
-
-                if (k != null) {
-
-                    arr[0] = nums[k];
-                    arr[1] = nums[j];
-
-                    List<Integer> l = new ArrayList<>();
-                    l.add(arr[0]);
-                    l.add(firstNum);
-                    l.add(arr[1]);
-                    Collections.sort(l);
-
-                    if (!results.contains(l)) {
-                        results.add(l);
-                    }
-
-
+            int start = i + 1, end = nums.length - 1, diff = -nums[i];
+            while (start < end) {                                   // O(N)
+                if (nums[start] + nums[end] == diff) {
+                    result.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    while (start < end && nums[start] == nums[start + 1]) start++;
+                    while (start < end && nums[end] == nums[end - 1]) end--;
+                    start++;
+                    end--;
+                } else if (nums[start] + nums[end] < diff) {
+                    start++;
                 } else {
-                    map.put(nums[j], j);
+                    end--;
                 }
             }
 
         }
-        return results;
+
+        return result;
+    }
+
+    // it is slower O(N^3)
+    private static List<List<Integer>> threesum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {         // O(N)
+            int diff = -nums[i];
+            Map<Integer, Integer> map = new HashMap();
+
+            for (int j = i + 1; j < nums.length; j++) {  // O(N)
+                int target = diff - nums[j];
+                Integer k = map.get(target);
+                if (k != null) {
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    Collections.sort(l);                   // O(Nlog(N)
+                    if (!result.contains(l)) {
+                        result.add(l);
+                    }
+                } else {
+                    map.put(nums[j], j);
+                }
+            }
+        }
+
+        return result;
     }
 }
