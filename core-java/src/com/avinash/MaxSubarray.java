e9277e5cf477252a77ab246e3b4414d3a3365da3
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-25 13:14:35 +0530

    LC 53 - Maximum Subarray

---
+++ core-java/src/com/avinash/MaxSubarray.java
@@ -0,0 +1,30 @@
+package com.avinash;
+
+/**
+ * https://leetcode.com/problems/maximum-subarray/
+ */
+public class MaxSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubArray(nums);
+        System.out.println(result);
+    }
+
+    private static int maxSubArray(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum = sum + nums[i];
+            }
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
