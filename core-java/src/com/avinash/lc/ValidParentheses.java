ddc3dfd63f6546328023d25bd890186421833163
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-26 21:26:00 +0530

    LC 20 - Valid Parentheses

---
+++ core-java/src/com/avinash/lc/ValidParentheses.java
@@ -0,0 +1,28 @@
+package com.avinash.lc;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String input = "()";
+        boolean result = isValid(input);
+
+        System.out.println(result);
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+
+        return stack.isEmpty();
+    }
+}
