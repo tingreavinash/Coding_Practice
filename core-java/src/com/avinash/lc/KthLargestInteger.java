2e37ce54782b0eaad8b15ec24dc494e22ae686cf
Avinash Tingre <avinash.tingre@cerence.com>
2021-08-29 13:51:23 +0530

    LC 1985 - Find the Kth Largest Integer in the Array

---
+++ core-java/src/com/avinash/lc/KthLargestInteger.java
@@ -0,0 +1,26 @@
+package com.avinash.lc;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/contest/weekly-contest-256/problems/find-the-kth-largest-integer-in-the-array/
+ */
+public class KthLargestInteger {
+    public static void main(String[] args) {
+        String[] nums = {"2", "21", "12", "1"};
+        int k = 3;
+        String result = kthLargestNumber(nums, k);
+        System.out.println(result);
+    }
+
+    private static String kthLargestNumber(String[] nums, int k) {
+        Arrays.sort(nums, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return Integer.compare(o1.length(), o2.length());
+        });
+
+        return nums[nums.length - k];
+    }
+}
