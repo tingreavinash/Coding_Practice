3deac97f7e6938e5589c1f4a2214b660543ec422
Avinash Tingre <avinash.tingre@cerence.com>
2021-11-06 13:04:01 +0530

    Use of transient variable

--- core-java/src/io/github/tingreavinash/java_concepts/serialization/Runner.java
+++ core-java/src/io/github/tingreavinash/java_concepts/serialization/Runner.java
@@ -3,8 +3,9 @@ package io.github.tingreavinash.java_concepts.serialization;
 import java.io.*;
 
 class Student implements Serializable {
+    transient int rollnum;
     String name;
-    int rollnum;
+
 
     Student(String name, int rollnum) {
         this.name = name;
@@ -13,9 +14,17 @@ class Student implements Serializable {
 }
 
 public class Runner {
+    static File file = new File("C:\\Personal\\My Project\\coding-practice\\core-java\\src\\io\\github\\tingreavinash\\java_concepts\\serialization\\object.out");
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //serializeObject();
+        deserializeObject();
+    }
+
+    public static void serializeObject() throws IOException {
         Student s1 = new Student("Avinash", 61);
-        File file = new File("C:\\Personal\\My Project\\coding-practice\\core-java\\src\\io\\github\\tingreavinash\\java_concepts\\serialization\\object.out");
+        System.out.println("Before serialization: " + s1.name + " " + s1.rollnum);
+
         FileOutputStream fout = new FileOutputStream(file);
         ObjectOutputStream out = new ObjectOutputStream(fout);
         out.writeObject(s1);
@@ -23,8 +32,9 @@ public class Runner {
         out.close();
         fout.close();
         System.out.println("object state persisted in file.");
+    }
 
-
+    public static void deserializeObject() throws IOException, ClassNotFoundException {
         FileInputStream fin = new FileInputStream(file);
         ObjectInputStream in = new ObjectInputStream(fin);
         Student s2 = (Student) in.readObject();
